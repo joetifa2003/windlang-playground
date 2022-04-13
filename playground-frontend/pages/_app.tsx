e8d91963bd9a4e118a9e0083f4090bd6d80940f0
joetifa2003 <joetifa2003@gmail.com>
2022-04-13 03:23:14 +0200

    feat: add goatcounter

--- playground-frontend/pages/_app.tsx
+++ playground-frontend/pages/_app.tsx
@@ -9,6 +9,11 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Navbar />
             <Component {...pageProps} />
             <Script src="https://code.iconify.design/2/2.2.1/iconify.min.js" />
+            <Script
+                data-goatcounter="https://windlang.goatcounter.com/count"
+                async
+                src="//gc.zgo.at/count.js"
+            ></Script>
         </>
     );
 }
